dadbcfa488b0331da1a288e2d2398a33e7865079
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-02-25 15:31:50 +0700

    feat: create state type for pokemon api; create zustand store to save url

---
+++ src/utils/store/pokemon-store.ts
@@ -0,0 +1,20 @@
+import {create} from 'zustand';
+
+type PokemonStoreType = {
+  currentPageUrl: string;
+  paginationNextUrl: string
+  paginationPrevUrl: string
+  setCurrentPageUrl: (currentPageUrl: string) => void;
+  setPaginationNextUrl: (paginationNextUrl: string) => void
+  setPaginationPrevUrl: (paginationPrevUrl: string) => void
+}
+
+export const PokemonStore = create<PokemonStoreType>((set)=>({
+  currentPageUrl: "https://pokeapi.co/api/v2/pokemon",
+  paginationNextUrl: "",
+  paginationPrevUrl: "",
+  setCurrentPageUrl: (currentPageUrl: string) => set({currentPageUrl}),
+  setPaginationPrevUrl: (paginationPrevUrl: string) => set({paginationPrevUrl}),
+  setPaginationNextUrl: (paginationNextUrl: string) => set({paginationNextUrl}),
+
+}))
---
+++ src/utils/type.ts
@@ -0,0 +1,11 @@
+export interface IResult {
+  count: number;
+  next: string;
+  previous: string;
+  results: IPokemon[]
+}
+
+export interface IPokemon {
+  name: string;
+  url: string;
+}
