2612ff808daa97b9370ee85686f2d1fd999b5864
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-02-25 11:01:50 +0700

    feat: create container and screen for HomeScreen.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,8 @@
-import { Text, View } from "react-native";
+import HomeScreen from "@/src/screen/HomeScreen"
 
-export default function Index() {
+const Index =()=> {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+   <HomeScreen />
   );
 }
+export default Index;
---
+++ src/screen/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from "@tamagui/core";
+
+const HomeScreen = () => {
+  return (
+    <View margin={10}>
+      <Text color="$color">Hello</Text>
+    </View>
+  )
+}
+
+export default HomeScreen;
