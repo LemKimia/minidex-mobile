d0251d591f2d3977d2ce639f54d2f7de3107566c
Samuel J T <155447567+LemKimia@users.noreply.github.com>
2025-02-26 10:02:08 +0700

    feat: add query client on root layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,22 @@
 import {Stack} from "expo-router";
-import { TamaguiProvider } from '@tamagui/core'
-import { config } from '@/tamagui.config'
+import {TamaguiProvider} from '@tamagui/core'
+import {config} from '@/tamagui.config'
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 const RootLayout = () => {
   return (
-    <TamaguiProvider config={config}><Stack><Stack.Screen name="index" options={{headerShown: false}}/></Stack></TamaguiProvider>
+    <TamaguiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{headerShown: false}}/>
+        </Stack>
+      </QueryClientProvider>
+    </TamaguiProvider>
   );
 }
 export default RootLayout;
